4542109f1f03b15f608084a39af566da2d327af0
Kemal Maulana <kemskems12@gmail.com>
2014-10-11 19:11:53 +0700

    Fix indentation (INDENT PAKAI 4 SPASI PUHLEASE)

--- main.cu
+++ main.cu
@@ -8,91 +8,89 @@
 
 __global__ void kernel(int * a)
 {
-	int idx = blockIdx.x*blockDim.x +threadIdx.x;
-	a[idx] = 7;
+    int idx = blockIdx.x*blockDim.x +threadIdx.x;
+    a[idx] = 7;
 }
-
 
 __global__ void kernelByThreadId(int * a)
 {
-	int idx = blockIdx.x*blockDim.x +threadIdx.x;
-	a[idx] = threadIdx.x;
+    int idx = blockIdx.x*blockDim.x +threadIdx.x;
+    a[idx] = threadIdx.x;
 }
 
 __global__ void kernelByBlockId(int * a)
 {
-	int idx = blockIdx.x*blockDim.x +threadIdx.x;
-	a[idx] = blockIdx.x;
+    int idx = blockIdx.x*blockDim.x +threadIdx.x;
+    a[idx] = blockIdx.x;
 }
-
-
 
 int main(void)
 {
-	int N = 10;
-	int *host_vector;
-	int *dev_vector;
+    int N = 10;
+    int *host_vector;
+    int *dev_vector;
 
 
-	dim3 blockSize(3,3,3);
-	dim3 gridSize(1,1);
+    dim3 blockSize(3,3,3);
+    dim3 gridSize(1,1);
 
-	//first experiment
-	host_vector = (int*) malloc(N*sizeof(int));
+    //first experiment
+    host_vector = (int*) malloc(N*sizeof(int));
 
-	for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
+    for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
 
-	cudaMalloc((void**)&dev_vector,N*sizeof(int));
+    cudaMalloc((void**)&dev_vector,N*sizeof(int));
 
-	cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
 
-	kernel<<<gridSize,blockSize>>>(dev_vector);
+    kernel<<<gridSize,blockSize>>>(dev_vector);
 
-	cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
 
-	printf("first experiment\n");
+    printf("first experiment\n");
 
-	for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
-	
-	free(host_vector);
-	cudaFree(dev_vector);
+    for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
 
-	//second experiment
-	host_vector = (int*) malloc(N*sizeof(int));
+    free(host_vector);
+    cudaFree(dev_vector);
 
-	for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
+    //second experiment
+    host_vector = (int*) malloc(N*sizeof(int));
 
-	cudaMalloc((void**)&dev_vector,N*sizeof(int));
+    for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
 
-	cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&dev_vector,N*sizeof(int));
 
-	kernelByBlockId<<<gridSize,blockSize>>>(dev_vector);
+    cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
 
-	cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
+    kernelByBlockId<<<gridSize,blockSize>>>(dev_vector);
 
-	printf("second experiment\n");
+    cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
 
-	for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
-	
-	free(host_vector);
-	cudaFree(dev_vector);
+    printf("second experiment\n");
 
-	//third experiment
-	host_vector = (int*) malloc(N*sizeof(int));
+    for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
 
-	for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
-	cudaMalloc((void**)&dev_vector,N*sizeof(int));
+    free(host_vector);
+    cudaFree(dev_vector);
 
-	cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
+    //third experiment
+    host_vector = (int*) malloc(N*sizeof(int));
 
-	kernelByThreadId<<<gridSize,blockSize>>>(dev_vector);
+    for(int ii = 0; ii < N ;ii++) host_vector[ii] = 0;
+    cudaMalloc((void**)&dev_vector,N*sizeof(int));
 
-	cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(dev_vector,host_vector,N*sizeof(int),cudaMemcpyHostToDevice);
 
-	printf("third experiment\n");
+    kernelByThreadId<<<gridSize,blockSize>>>(dev_vector);
 
-	for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
-	
-	free(host_vector);
-	cudaFree(dev_vector);
+    cudaMemcpy(host_vector,dev_vector,N*sizeof(int),cudaMemcpyDeviceToHost);
+
+    printf("third experiment\n");
+
+    for(int ii = 0; ii < N ;ii++)	printf("%d \n",host_vector[ii]);
+
+    free(host_vector);
+    cudaFree(dev_vector);
 }
+
